5fa0dde35bea7eb68c413ebbe3de1e764c3bae48
Gearrion <Gearrion@gmail.com>
2016-04-01 18:38:18 -0400

    updated

--- EnderLegacyMod/src/java/mod/Gearrion/EnderLegacymod/recipes/ModRecipe.java
+++ EnderLegacyMod/src/java/mod/Gearrion/EnderLegacymod/recipes/ModRecipe.java
@@ -3,6 +3,8 @@ package mod.Gearrion.EnderLegacymod.recipes;
 import cpw.mods.fml.common.registry.GameRegistry;
 import mod.Gearrion.EnderLegacymod.block.ModBlocks;
 import mod.Gearrion.EnderLegacymod.item.ModItems;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
@@ -22,6 +24,18 @@ import net.minecraftforge.oredict.OreDictionary;
 		   new ItemStack(ModItems.Endersteelignot, 9),
 		   new ItemStack(ModBlocks.Endersteelblock));	
 		
-	}
-
+	//Tools
+		GameRegistry.addRecipe(new ItemStack(ModItems.Endersteelpickaxe, 1),"eee"," o "," o ",'e', ModItems.Endersteelignot,'o', Item.itemRegistry.getObject("stick"));
+		GameRegistry.addRecipe(new ItemStack(ModItems.Endersteelaxe, 1),"ee ",	"eo "," o ",'e', ModItems.Endersteelignot,'o', Item.itemRegistry.getObject("stick"));
+		
+	//Weapons
+		GameRegistry.addRecipe(new ItemStack(ModItems.EnderBlade, 1),"eee",	" i "," o ",'e', ModItems.Endersteelignot,'o', Item.itemRegistry.getObject("stick"),'i', Items.ender_eye);
+		GameRegistry.addRecipe(new ItemStack(ModItems.Endersteelsword, 1)," e "," e "," o ",'e', ModItems.Endersteelignot,'o', Item.itemRegistry.getObject("stick"));
+  
+	//Armor	
+		GameRegistry.addRecipe(new ItemStack(ModItems.Endersteelhelmet, 1),"eee","e e","   ",'e', ModItems.Endersteelignot);
+		GameRegistry.addRecipe(new ItemStack(ModItems.Endersteelchestplate, 1),"e e","eee","eee",'e', ModItems.Endersteelignot);
+		GameRegistry.addRecipe(new ItemStack(ModItems.Endersteelleggings, 1),"eee","e e","e e",'e', ModItems.Endersteelignot);
+		GameRegistry.addRecipe(new ItemStack(ModItems.Endersteelboots, 1),"   ","e e","e e",'e', ModItems.Endersteelignot);
+		}
 }
