12c15d36b21701796cf0b94b2933e64acbf9bea9
Gearrion <Gearrion@users.noreply.github.com>
2016-05-10 08:15:31 -0400

    Update ModItems.java

--- EnderLegacyMod/src/java/mod/Gearrion/EnderLegacymod/Items/ModItems.java
+++ EnderLegacyMod/src/java/mod/Gearrion/EnderLegacymod/Items/ModItems.java
@@ -34,7 +34,8 @@ public final class ModItems {
 		public static ToolMaterial MatEnderSteelItem = EnumHelper.addToolMaterial("MatEnderSteelItem", 4, 2500, 14.0F, 6.0F, 30);
 		public static ToolMaterial MatEnderBladeItem = EnumHelper.addToolMaterial("MatEnderBladeItem", 4, 3000, 16.0F, 8.0F, 40);
 		public static ArmorMaterial MatEnderSteelArmor = EnumHelper.addArmorMaterial("MatEnderSteelArmor", 16, new int[] {3, 8, 6, 3}, 30);	
-	
+		public static ToolMaterial MatEnderwand = EnumHelper.addToolMaterial("MatEnderwand", 1, 50, 1.0F, 1.0F, 0);	
+		
   public static final void init() {
 		
 	    //ITEMS
@@ -43,7 +44,7 @@ public final class ModItems {
 		GameRegistry.registerItem(Endersteelignot = new ItemModBasic("Endersteelignot"),"Endersteelignot");
 	    
 		//Wands
-		GameRegistry.registerItem(ignighterWand = new ignighterWand("Enderwand"),"Enderwand");
+		GameRegistry.registerItem(ignighterWand = new ignighterWand(MatEnderwand, "Enderwand"),"Enderwand");
 		
 		//Armor
         GameRegistry.registerItem(Endersteelhelmet = new ItemModArmor("Endersteelhelmet", MatEnderSteelArmor, "Endersteelhelmet", 0), "Endersteelhelmet"); //0 for helmet
