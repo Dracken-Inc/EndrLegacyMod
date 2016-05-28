b5ce13073a28dd67238376a735b45211292dbd8b
Henry Lafontaine <Gearrion@gmail.com>
2016-05-28 10:34:30 -0400

    Gen Layer added for Tree's

---
+++ EnderLegacyMod/src/java/mod/Gearrion/EnderLegacymod/world/GenLayer/EndForestGenLayerBiomes.java
@@ -0,0 +1,35 @@
+package mod.Gearrion.EnderLegacymod.world.GenLayer;
+
+import mod.Gearrion.EnderLegacymod.world.biome.BiomeRegistry;
+import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.gen.layer.GenLayer;
+import net.minecraft.world.gen.layer.IntCache;
+
+public class EndForestGenLayerBiomes extends GenLayer {
+
+	// TODO: spawning with temperatures
+    protected BiomeGenBase[] allowedBiomes = {
+    		BiomeRegistry.biomeTrueEnd,
+    };
+    
+    public EndForestGenLayerBiomes(long seed) {
+		super(seed);
+	}
+	
+	public EndForestGenLayerBiomes(long seed, GenLayer genlayer) {
+		super(seed);
+		this.parent = genlayer;
+	}
+	
+    @Override
+    public int[] getInts(int x, int z, int width, int depth) {
+        int[] dest = IntCache.getIntCache(width * depth);
+        for (int dz = 0; dz < depth; dz++) {
+            for (int dx = 0; dx < width; dx++) {
+                this.initChunkSeed(dx + x, dz + z);
+                dest[(dx + dz * width)] = this.allowedBiomes[nextInt(this.allowedBiomes.length)].biomeID;
+            }
+        }
+        return dest;
+    }
+}
