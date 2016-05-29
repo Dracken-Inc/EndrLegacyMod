7c65540dd0909f6d01603175719f76d298a8371a
Henry Lafontaine <Gearrion@gmail.com>
2016-05-29 08:06:25 -0400

    Added Util folder with helper classes for loggin and debugging.

---
+++ EnderLegacyMod/src/java/mod/Gearrion/EnderLegacymod/Util/Details.java
@@ -0,0 +1,12 @@
+package mod.Gearrion.EnderLegacymod.Util;
+
+
+/**
+ * Details.class holds basic stuff for the Mod like vesrion, name, title etc.
+ * @author Jimmy04Creeper
+ */
+public abstract class Details {
+    public static final String MODID = "EnderLegacyMod";
+    public static final String MODNAME = "Ender Legacy Mod";
+    public static final String VERSION = "1.0.8";
+}
---
+++ EnderLegacyMod/src/java/mod/Gearrion/EnderLegacymod/Util/Names.java
@@ -0,0 +1,6 @@
+package mod.Gearrion.EnderLegacymod.Util;
+
+public class Names {
+
+	public static final String FORESTBIOMENAME = "True End";
+}
